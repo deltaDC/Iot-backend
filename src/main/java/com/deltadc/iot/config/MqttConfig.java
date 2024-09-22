e707a9163f6955bec659ceb99d2dba1c672dce88
Đức Chính Phạm <ducchinh.work@gmail.com>
2024-09-22 22:01:48 +0700

    add mqtt authentication and change port to 1884

--- src/main/java/com/deltadc/iot/config/MqttConfig.java
+++ src/main/java/com/deltadc/iot/config/MqttConfig.java
@@ -5,6 +5,7 @@ import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +13,15 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 public class MqttConfig {
 
-    private static final String BROKER_URL = "tcp://localhost:1883";
+    @Value("${mqtt.username}")
+    private String MQTT_USERNAME;
+
+    @Value("${mqtt.password}")
+    private String MQTT_PASSWORD;
+
+    @Value("${mqtt.broker.url}")
+    private String BROKER_URL;
+
     private static final String CLIENT_ID = "spring-client";
 
     @Bean
@@ -23,6 +32,8 @@ public class MqttConfig {
         options.setCleanSession(true);
         options.setAutomaticReconnect(true);
         options.setConnectionTimeout(10);
+        options.setUserName(MQTT_USERNAME);
+        options.setPassword(MQTT_PASSWORD.toCharArray());
 
         client.connect(options);
         return client;
